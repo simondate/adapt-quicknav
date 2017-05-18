1ef12528bd1a5e3343e4a8027209216356a3eba2
Oliver Foster <oliver.foster@kineo.com>
2017-05-18 12:32:09 +0100

    issue/25: tabs to spaces

--- js/adapt-quicknav.js
+++ js/adapt-quicknav.js
@@ -1,149 +1,149 @@
 define([
-	'core/js/adapt',
-	'./adapt-quicknavPageView',
-	'./quicknav-placeholder'
+    'core/js/adapt',
+    './adapt-quicknavPageView',
+    './quicknav-placeholder'
 ], function(Adapt, QuickNavPageView) {
 
-	var QuickNav = Backbone.Controller.extend({
+    var QuickNav = Backbone.Controller.extend({
 
-		config: undefined,
+        config: undefined,
 
-		state: {
-			lastBlock: undefined,
-			currentMenu: undefined,
-			currentPage: undefined,
-			isFirstPage: false,
-			isLastPage: false,
-			menuPages: {}
-		},
+        state: {
+            lastBlock: undefined,
+            currentMenu: undefined,
+            currentPage: undefined,
+            isFirstPage: false,
+            isLastPage: false,
+            menuPages: {}
+        },
 
-		initialize: function() {
-			this.listenTo(Adapt, {
-				"app:dataReady": this.onDataReady,
-				"pageView:postRender": this.onPagePostRender,
-				"blockView:postRender": this.onBlockPostRender
-			});
-		},
+        initialize: function() {
+            this.listenTo(Adapt, {
+                "app:dataReady": this.onDataReady,
+                "pageView:postRender": this.onPagePostRender,
+                "blockView:postRender": this.onBlockPostRender
+            });
+        },
 
-		onDataReady: function() {
+        onDataReady: function() {
 
-			var menus = [Adapt.course].concat(Adapt.contentObjects.where({_type: "menu"}));
+            var menus = [Adapt.course].concat(Adapt.contentObjects.where({_type: "menu"}));
 
-			_.each(menus, _.bind(function(menu) {
+            _.each(menus, _.bind(function(menu) {
 
-				var id = menu.get("_id");
-				var pages = Adapt.contentObjects.where({_type: "page", _parentId: id });
-				
-				this.state.menuPages[id] = {};
+                var id = menu.get("_id");
+                var pages = Adapt.contentObjects.where({_type: "page", _parentId: id });
+                
+                this.state.menuPages[id] = {};
 
-				_.each(pages, _.bind(function(page) {
-					this.state.menuPages[id][page.get("_id")] = page;
-				}, this));
+                _.each(pages, _.bind(function(page) {
+                    this.state.menuPages[id][page.get("_id")] = page;
+                }, this));
 
-			}, this));
+            }, this));
 
-		},
+        },
 
-		onPagePostRender: function(pageView) {
+        onPagePostRender: function(pageView) {
 
-			var pageModel = pageView.model;
-			this.config = pageModel.get("_quicknav");
+            var pageModel = pageView.model;
+            this.config = pageModel.get("_quicknav");
 
-			if (this.config === undefined || (this.config._isEnabled !== true && this.config._isEnabled !== undefined)) {
-				return;
-			}
+            if (this.config === undefined || (this.config._isEnabled !== true && this.config._isEnabled !== undefined)) {
+                return;
+            }
 
-			var blocks = pageModel.findDescendants("blocks");
-			var parentId = pageModel.get("_parentId");
+            var blocks = pageModel.findDescendants("blocks");
+            var parentId = pageModel.get("_parentId");
 
-			this.state.currentMenu = Adapt.findById(parentId);
-			this.state.currentPage = pageModel;
-			this.state.lastBlock = blocks.last();
+            this.state.currentMenu = Adapt.findById(parentId);
+            this.state.currentPage = pageModel;
+            this.state.lastBlock = blocks.last();
 
-		},
+        },
 
-		onBlockPostRender: function(blockView) {
+        onBlockPostRender: function(blockView) {
 
-			if (this.state.lastBlock === undefined) return;
-			if (blockView.model.get("_id") !== this.state.lastBlock.get("_id")) return;
+            if (this.state.lastBlock === undefined) return;
+            if (blockView.model.get("_id") !== this.state.lastBlock.get("_id")) return;
 
-			var element = blockView.$el.parent();
+            var element = blockView.$el.parent();
 
-			this.updateState();
+            this.updateState();
 
-			var quickNavPageView = new QuickNavPageView({
-				model: this.state.currentPage,
-				config: this.config, 
-				state: this.state,
-				parent: this
-			});
-			quickNavPageView.undelegateEvents();
+            var quickNavPageView = new QuickNavPageView({
+                model: this.state.currentPage,
+                config: this.config, 
+                state: this.state,
+                parent: this
+            });
+            quickNavPageView.undelegateEvents();
 
-			var injectInto;
-			if (this.config._injectIntoSelector) {
-				injectInto = element.find(this.config._injectIntoSelector);
-				if (injectInto.length > 0) {
-					injectInto.append(quickNavPageView.$el);
-				} else {
-					element.append(quickNavPageView.$el);
-				}
-			} else {
-				injectInto = element.find(".quicknav-component");
-				if (injectInto.length > 0) {
-					injectInto.append(quickNavPageView.$el);
-				} else {
-					element.append(quickNavPageView.$el);
-				}
-			}
-			
-			quickNavPageView.delegateEvents();
+            var injectInto;
+            if (this.config._injectIntoSelector) {
+                injectInto = element.find(this.config._injectIntoSelector);
+                if (injectInto.length > 0) {
+                    injectInto.append(quickNavPageView.$el);
+                } else {
+                    element.append(quickNavPageView.$el);
+                }
+            } else {
+                injectInto = element.find(".quicknav-component");
+                if (injectInto.length > 0) {
+                    injectInto.append(quickNavPageView.$el);
+                } else {
+                    element.append(quickNavPageView.$el);
+                }
+            }
+            
+            quickNavPageView.delegateEvents();
 
-		},
+        },
 
-		updateState: function() {
+        updateState: function() {
 
-			this.state.isFirstPage = false;
-			this.state.isLastPage = false;
+            this.state.isFirstPage = false;
+            this.state.isLastPage = false;
 
-			var pages;
+            var pages;
 
-			if (_.keys(this.state.menuPages).length === 0) {
-				pages = _.pluck(new Backbone.Collection(Adapt.contentObjects.where({_type: "page", _isAvailable: true})).toJSON(), [ "_id" ]);
-			} else if (!this.state.currentMenu) {
-				this.state.isFirstPage = true;
-				this.state.isLastPage = true;
-				return;
-			} else {
-				pages = _.keys(this.state.menuPages[this.state.currentMenu.get("_id")]);
-				
-				pages = _.reject(pages, function(id) {
-					var model = Adapt.contentObjects.findWhere({_id: id});
-					return !model.get('_isAvailable');
-				});
-			}
+            if (_.keys(this.state.menuPages).length === 0) {
+                pages = _.pluck(new Backbone.Collection(Adapt.contentObjects.where({_type: "page", _isAvailable: true})).toJSON(), [ "_id" ]);
+            } else if (!this.state.currentMenu) {
+                this.state.isFirstPage = true;
+                this.state.isLastPage = true;
+                return;
+            } else {
+                pages = _.keys(this.state.menuPages[this.state.currentMenu.get("_id")]);
+                
+                pages = _.reject(pages, function(id) {
+                    var model = Adapt.contentObjects.findWhere({_id: id});
+                    return !model.get('_isAvailable');
+                });
+            }
 
-			if (pages === undefined) return;
+            if (pages === undefined) return;
 
-			var indexOfPage = _.indexOf(pages, this.state.currentPage.get("_id"));
-			
-			if (this.config._isContinuous == "local" || this.config._isContinuous == "global") {
-				if (indexOfPage === 0 && indexOfPage == pages.length - 1 && this.config._isContinuous == "local") {
-					this.state.isFirstPage = true;
-					this.state.isLastPage = true;
-				} else {
-					this.state.isLastPage = false;
-					this.state.isFirstPage = false;
-				}
-			} else {
-				if (indexOfPage === 0) this.state.isFirstPage = true;
-				if (indexOfPage == pages.length - 1) this.state.isLastPage = true;
-			}
+            var indexOfPage = _.indexOf(pages, this.state.currentPage.get("_id"));
+            
+            if (this.config._isContinuous == "local" || this.config._isContinuous == "global") {
+                if (indexOfPage === 0 && indexOfPage == pages.length - 1 && this.config._isContinuous == "local") {
+                    this.state.isFirstPage = true;
+                    this.state.isLastPage = true;
+                } else {
+                    this.state.isLastPage = false;
+                    this.state.isFirstPage = false;
+                }
+            } else {
+                if (indexOfPage === 0) this.state.isFirstPage = true;
+                if (indexOfPage == pages.length - 1) this.state.isLastPage = true;
+            }
 
-		}
-	});
+        }
+    });
 
-	Adapt.quicknav = new QuickNav();
+    Adapt.quicknav = new QuickNav();
 
-	return QuickNav;
+    return QuickNav;
 
 });
--- js/adapt-quicknavPageView.js
+++ js/adapt-quicknavPageView.js
@@ -1,52 +1,52 @@
 define([
-	'core/js/adapt'
+    'core/js/adapt'
 ], function(Adapt) {
 
-	var QuickNavPageView = Backbone.View.extend({
-		
-		className: "block quicknav",
+    var QuickNavPageView = Backbone.View.extend({
+        
+        className: "block quicknav",
 
-		events: {
-			"click #root": "onRoot",
-			"click #previous": "onPrevious",
-			"click #up": "onUp",
-			"click #next": "onNext"
-		},
+        events: {
+            "click #root": "onRoot",
+            "click #previous": "onPrevious",
+            "click #up": "onUp",
+            "click #next": "onNext"
+        },
 
-		initialize: function(options) {
+        initialize: function(options) {
 
-			this.state = options.state;
-			this.config = options.config;
-			this.parent = options.parent;
+            this.state = options.state;
+            this.config = options.config;
+            this.parent = options.parent;
 
-			this.listenTo(Adapt, 'remove', this.remove);
+            this.listenTo(Adapt, 'remove', this.remove);
 
-			this.render();
+            this.render();
 
-			this.setLocking();
-			this.enableNextOnCompletion();
+            this.setLocking();
+            this.enableNextOnCompletion();
 
-		},
+        },
 
-		render: function() {
+        render: function() {
 
-	        var template = Handlebars.templates["quicknav-bar"];
+            var template = Handlebars.templates["quicknav-bar"];
 
-	        var data = this.model.toJSON();
-	        data.state = this.state;
-	        data.config = this.config;
+            var data = this.model.toJSON();
+            data.state = this.state;
+            data.config = this.config;
 
-	        this.$el.html(template(data));
+            this.$el.html(template(data));
 
-	        return this;
+            return this;
 
-		},
+        },
 
-		setLocking: function() {
+        setLocking: function() {
 
-	        this.state._locked = false;
+            this.state._locked = false;
 
-	        var config = this.config;
+            var config = this.config;
             if (!config._lock) return;
 
             var contentObjects = config._lock;
@@ -54,15 +54,15 @@ define([
 
             for (var i = 0; i < contentObjects.length; i++) {
 
-  				var contentObject = Adapt.contentObjects.findWhere({
-  					_id:contentObjects[i]
-  				});
+                var contentObject = Adapt.contentObjects.findWhere({
+                    _id:contentObjects[i]
+                });
 
-  				var isComplete = contentObject.get("_isComplete");
-  				var isAvailable = contentObject.get("_isAvailable");
-  				if (!isComplete && isAvailable) continue;
-            	
-            	completeCount++;
+                var isComplete = contentObject.get("_isComplete");
+                var isAvailable = contentObject.get("_isAvailable");
+                if (!isComplete && isAvailable) continue;
+                
+                completeCount++;
 
             }
 
@@ -70,179 +70,179 @@ define([
             
             this.state._locked = true;
 
-		},
+        },
 
-		enableNextOnCompletion: function() {
+        enableNextOnCompletion: function() {
 
-			if (!this.config._isEnableNextOnCompletion) return;
-				
-			if (this.model.get("_isComplete")) {
-				this.onPageCompleted();
-			} else {
-				this.listenTo(this.model, "change:_isComplete", this.onPageCompleted);
-			}
-
-			if (this.state._locked === true) {
-            	this.$('#next').attr("disabled", "disabled");
+            if (!this.config._isEnableNextOnCompletion) return;
+                
+            if (this.model.get("_isComplete")) {
+                this.onPageCompleted();
             } else {
-            	this.$('#next').removeAttr("disabled");
+                this.listenTo(this.model, "change:_isComplete", this.onPageCompleted);
             }
 
-		},
+            if (this.state._locked === true) {
+                this.$('#next').attr("disabled", "disabled");
+            } else {
+                this.$('#next').removeAttr("disabled");
+            }
 
-		onRoot: function() {
-			this.navigateTo("/");
-		},
+        },
 
-		onPrevious:function() {
-			var params = this.getParameters();
+        onRoot: function() {
+            this.navigateTo("/");
+        },
 
-			if (params.pages === undefined) return;
+        onPrevious:function() {
+            var params = this.getParameters();
 
-			var indexOfCurrentPage = _.indexOf(params.pages, this.model.get("_id"));
-			var indexOfPrevPage = this.getPrevPageIndex(params.menus, params.indexOfMenu, params.pages, indexOfCurrentPage);
+            if (params.pages === undefined) return;
 
-			this.navigateTo(params.pages[indexOfPrevPage]);
-		},
+            var indexOfCurrentPage = _.indexOf(params.pages, this.model.get("_id"));
+            var indexOfPrevPage = this.getPrevPageIndex(params.menus, params.indexOfMenu, params.pages, indexOfCurrentPage);
 
-		onUp: function() {
-			this.navigateTo(this.model.get("_parentId"));
-		},
+            this.navigateTo(params.pages[indexOfPrevPage]);
+        },
 
-		onNext: function() {
-			var params = this.getParameters();
+        onUp: function() {
+            this.navigateTo(this.model.get("_parentId"));
+        },
 
-			if (params.pages === undefined) return;
+        onNext: function() {
+            var params = this.getParameters();
 
-			var indexOfCurrentPage = _.indexOf(params.pages, this.model.get("_id"));
-			var indexOfNextPage = this.getNextPageIndex(params.menus, params.indexOfMenu, params.pages, indexOfCurrentPage);
+            if (params.pages === undefined) return;
 
-			this.navigateTo(params.pages[indexOfNextPage]);
-		},
+            var indexOfCurrentPage = _.indexOf(params.pages, this.model.get("_id"));
+            var indexOfNextPage = this.getNextPageIndex(params.menus, params.indexOfMenu, params.pages, indexOfCurrentPage);
 
-		getParameters: function() {
+            this.navigateTo(params.pages[indexOfNextPage]);
+        },
 
-			var params = {};
+        getParameters: function() {
 
-			if (_.keys(this.state.menuPages).length === 0 || !this.state.currentMenu) {
-				params.pages = _.pluck(new Backbone.Collection(Adapt.contentObjects.where({_type: "page"})).toJSON(), [ "_id" ]);
-				return params;
-			}
+            var params = {};
 
-			params.menus = _.keys(this.state.menuPages);
-			params.indexOfMenu = _.indexOf(params.menus, this.state.currentMenu.get("_id"));
-			params.pages = _.keys(this.state.menuPages[this.state.currentMenu.get("_id")]);
+            if (_.keys(this.state.menuPages).length === 0 || !this.state.currentMenu) {
+                params.pages = _.pluck(new Backbone.Collection(Adapt.contentObjects.where({_type: "page"})).toJSON(), [ "_id" ]);
+                return params;
+            }
 
-			return params;
-		},
+            params.menus = _.keys(this.state.menuPages);
+            params.indexOfMenu = _.indexOf(params.menus, this.state.currentMenu.get("_id"));
+            params.pages = _.keys(this.state.menuPages[this.state.currentMenu.get("_id")]);
 
-		navigateTo: function(id) {
+            return params;
+        },
 
-			if (id === Adapt.course.get("_id")) {
-				id = "/";
-			}
+        navigateTo: function(id) {
 
-			var hash = "#" + (id === "/" ? id : "/id/" + id);
-			Backbone.history.navigate(hash, {trigger:true, "replace": false});
+            if (id === Adapt.course.get("_id")) {
+                id = "/";
+            }
 
-		},
+            var hash = "#" + (id === "/" ? id : "/id/" + id);
+            Backbone.history.navigate(hash, {trigger:true, "replace": false});
 
-		onPageCompleted: function() {
-			this.setLocking();
-		},
+        },
 
-		getPrevPageIndex: function(menus, indexOfMenu, pages, indexOfPage) {
+        onPageCompleted: function() {
+            this.setLocking();
+        },
 
-			if (this.config._isContinuous == "global" && (menus !== undefined || !this.state.currentMenu)) {
-				if (indexOfPage === 0 || !this.state.currentMenu) { //if page is at the beginning of the menu goto previous menu, last page
-					if (this.config._global !== undefined && this.config._global._pagePrevious !== undefined) {
-						this.navigateTo(this.config._global._pagePrevious);
-						return;
-					} else if (indexOfMenu === 0 && indexOfMenu == menus.length - 1) {
-						//single menu, last page
-					} else if (indexOfMenu === 0) {
-						indexOfMenu = menus.length - 1; //last menu
-						pages = _.keys(this.state.menuPages[menus[indexOfMenu]]); //get menu pages
-					} else {
-						indexOfMenu-=1; //previous menu
-						pages = _.keys(this.state.menuPages[menus[indexOfMenu]]); //get menu pages
-					}
-					indexOfPage = pages.length - 1; //last page
-				} else {
-					indexOfPage-=1; //previous page
-				}
-			} else if (this.config._isContinuous == "local" || (this.config._isContinuous == "global" && menus === undefined)) {
-				if (indexOfPage === 0 && indexOfPage == pages.length - 1) {
-					//single page
-				} else if (indexOfPage === 0) {
-					indexOfPage = pages.length - 1; //last page
-				} else {
-					indexOfPage-=1; //previous page
-				}
-			} else {
-				if (indexOfPage === 0) return;
-				indexOfPage-=1; //previous page
-			}
+        getPrevPageIndex: function(menus, indexOfMenu, pages, indexOfPage) {
 
-			if (this.isPageAvailable(pages, indexOfPage)) {
-				return indexOfPage;
-			} else {
-				//uh-oh, that page isn't available - try again
-				return this.getPrevPageIndex(menus, indexOfMenu, pages, indexOfPage);
-			}
+            if (this.config._isContinuous == "global" && (menus !== undefined || !this.state.currentMenu)) {
+                if (indexOfPage === 0 || !this.state.currentMenu) { //if page is at the beginning of the menu goto previous menu, last page
+                    if (this.config._global !== undefined && this.config._global._pagePrevious !== undefined) {
+                        this.navigateTo(this.config._global._pagePrevious);
+                        return;
+                    } else if (indexOfMenu === 0 && indexOfMenu == menus.length - 1) {
+                        //single menu, last page
+                    } else if (indexOfMenu === 0) {
+                        indexOfMenu = menus.length - 1; //last menu
+                        pages = _.keys(this.state.menuPages[menus[indexOfMenu]]); //get menu pages
+                    } else {
+                        indexOfMenu-=1; //previous menu
+                        pages = _.keys(this.state.menuPages[menus[indexOfMenu]]); //get menu pages
+                    }
+                    indexOfPage = pages.length - 1; //last page
+                } else {
+                    indexOfPage-=1; //previous page
+                }
+            } else if (this.config._isContinuous == "local" || (this.config._isContinuous == "global" && menus === undefined)) {
+                if (indexOfPage === 0 && indexOfPage == pages.length - 1) {
+                    //single page
+                } else if (indexOfPage === 0) {
+                    indexOfPage = pages.length - 1; //last page
+                } else {
+                    indexOfPage-=1; //previous page
+                }
+            } else {
+                if (indexOfPage === 0) return;
+                indexOfPage-=1; //previous page
+            }
 
-		},
+            if (this.isPageAvailable(pages, indexOfPage)) {
+                return indexOfPage;
+            } else {
+                //uh-oh, that page isn't available - try again
+                return this.getPrevPageIndex(menus, indexOfMenu, pages, indexOfPage);
+            }
 
-		getNextPageIndex: function(menus, indexOfMenu, pages, indexOfPage) {
+        },
 
-			if (this.config._isContinuous == "global" && (menus !== undefined || !this.state.currentMenu)) {
-				if (indexOfPage === pages.length - 1 || !this.state.currentMenu) { //if page is at the end of the menu goto next menu, first page
-					if (this.config._global !== undefined && this.config._global._pageNext !== undefined) {
-						this.navigateTo(this.config._global._pageNext);
-						return;
-					} else if (indexOfMenu === 0 && indexOfMenu == menus.length - 1) {
-						//single menu, first page
-					} else if (indexOfMenu == menus.length - 1) {
-						indexOfMenu = 0; //first menu
-						pages = _.keys(this.state.menuPages[menus[indexOfMenu]]); //get menu pages
-					} else {
-						indexOfMenu+=1; //next menu
-						pages = _.keys(this.state.menuPages[menus[indexOfMenu]]); //get menu pages
-					}
-					indexOfPage = 0; //first page
-				} else {
-					indexOfPage+=1; //next page
-				}
-			} else if (this.config._isContinuous == "local" || (this.config._isContinuous == "global" && menus === undefined)) {
-				if (indexOfPage === 0 && indexOfPage == pages.length - 1) {
-					//single page
-				} else if (indexOfPage == pages.length - 1) {
-					indexOfPage = 0; //first page
-				} else {
-					indexOfPage+=1; //next page
-				}
-			} else {
-				if (indexOfPage == pages.length - 1) return;
-				indexOfPage+=1; //next page
-			}
+        getNextPageIndex: function(menus, indexOfMenu, pages, indexOfPage) {
 
-			if (this.isPageAvailable(pages, indexOfPage)) {
-				return indexOfPage;
-			} else {
-				//uh-oh, that page isn't available - try again
-				return this.getNextPageIndex(menus, indexOfMenu, pages, indexOfPage);
-			}
+            if (this.config._isContinuous == "global" && (menus !== undefined || !this.state.currentMenu)) {
+                if (indexOfPage === pages.length - 1 || !this.state.currentMenu) { //if page is at the end of the menu goto next menu, first page
+                    if (this.config._global !== undefined && this.config._global._pageNext !== undefined) {
+                        this.navigateTo(this.config._global._pageNext);
+                        return;
+                    } else if (indexOfMenu === 0 && indexOfMenu == menus.length - 1) {
+                        //single menu, first page
+                    } else if (indexOfMenu == menus.length - 1) {
+                        indexOfMenu = 0; //first menu
+                        pages = _.keys(this.state.menuPages[menus[indexOfMenu]]); //get menu pages
+                    } else {
+                        indexOfMenu+=1; //next menu
+                        pages = _.keys(this.state.menuPages[menus[indexOfMenu]]); //get menu pages
+                    }
+                    indexOfPage = 0; //first page
+                } else {
+                    indexOfPage+=1; //next page
+                }
+            } else if (this.config._isContinuous == "local" || (this.config._isContinuous == "global" && menus === undefined)) {
+                if (indexOfPage === 0 && indexOfPage == pages.length - 1) {
+                    //single page
+                } else if (indexOfPage == pages.length - 1) {
+                    indexOfPage = 0; //first page
+                } else {
+                    indexOfPage+=1; //next page
+                }
+            } else {
+                if (indexOfPage == pages.length - 1) return;
+                indexOfPage+=1; //next page
+            }
 
-		},
+            if (this.isPageAvailable(pages, indexOfPage)) {
+                return indexOfPage;
+            } else {
+                //uh-oh, that page isn't available - try again
+                return this.getNextPageIndex(menus, indexOfMenu, pages, indexOfPage);
+            }
 
-		isPageAvailable: function(pages, indexOfPage) {
+        },
 
-			var contentObject = Adapt.findById(pages[indexOfPage]);
-			return contentObject.get('_isAvailable');
-			
-		}
+        isPageAvailable: function(pages, indexOfPage) {
 
-	});
+            var contentObject = Adapt.findById(pages[indexOfPage]);
+            return contentObject.get('_isAvailable');
+            
+        }
 
-	return QuickNavPageView;
+    });
+
+    return QuickNavPageView;
 });
